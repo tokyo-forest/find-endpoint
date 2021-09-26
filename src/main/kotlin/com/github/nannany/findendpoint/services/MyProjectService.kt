379205ccedebe9b73837f6aebc3df6e40b86d583
GitHub Action <action@github.com>
2021-09-26 05:48:57 +0000

    Template cleanup

--- src/main/kotlin/com/github/nannany/findendpoint/services/MyProjectService.kt
+++ src/main/kotlin/com/github/nannany/findendpoint/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.nannany.findendpoint.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.nannany.findendpoint.MyBundle
 
 class MyProjectService(project: Project) {
 
